43dd88be60c7e863c2b21a3f7b44d739c20a582e
Jonny K <Kempj2.jk@gmail.com>
2020-09-29 10:00:41 -0700

    Adding a contact form. Modal setup need to still hook up to email and finalize

--- script.js
+++ script.js
@@ -36,8 +36,35 @@ $(document).ready(function(){
         
         
     })
+   
     $(".date-since").each(function(){
         $(this).text((moment($(this).text(), "YYYYMMDD").fromNow()));
+    })
+
+    // Get the modal
+    var modal = $("#myModal");
+
+    // Get the button that opens the modal
+    var btn = $(".trigger");
+
+    // Get the <span> element that closes the modal
+    var span = $(".close");
+
+    // When the user clicks on the button, open the modal
+    btn.on("click", function() {
+    modal.css({"display": "block"});
+    })
+
+    // When the user clicks on <span> (x), close the modal
+    span.on("click", function() {
+        modal.css({"display": "none"});
+    })
+
+    // When the user clicks anywhere outside of the modal, close it
+    $(window).on("click" , function(event) {
+    if (event.target == modal) {
+        modal.css({"display": "none"});
+    }
     })
 
     
